41ff0e6bccb325edfc239962af73bc7e2fab9948
acd74982306 <114900371+acd74982306@users.noreply.github.com>
2022-10-30 20:17:27 -0500

    added criteria prompts and verification that at least one is selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,33 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
   var userInput = window.prompt("Choose password length between 8 & 128 characters");
+  // Convert string to number
   var passwordLength = Number(userInput);
 
-  // Test and reprompt user if password length is an invalid value
+  // Test if password length is a valid value
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    if (isNaN(passwordLength)) {window.alert("That is not a number!")}
-    else {window.alert("Length must be between 8 and 128")}
+
+    if (isNaN(passwordLength)) { window.alert("That is not a number!") }
+    else { window.alert("Length must be between 8 and 128") }
+    // Exit generatePassword function because invalid input
     return;
   }
+
+  window.alert("Choose your password criteria ... ")
+  // Record user input to variables as boolean values
+  var includeLower = window.confirm("Include lowercase letters?");
+  var includeUpper = window.confirm("Include UPPERCASE letters?");
+  var includeNumbers = window.confirm("Include numbers?");
+  var includeSymbols = window.confirm("Include symbols?");
+
+  // Verify user has selected at least one char set
+  if (!includeLower && !includeUpper && !includeNumbers && !includeSymbols) {
+    window.alert("At least one character set is required!");
+    return;
+  }
+
+
+
 
 }
 
