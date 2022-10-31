ddb0fb002118bdf8a36381b11611c4970eaa5177
acd74982306 <114900371+acd74982306@users.noreply.github.com>
2022-10-30 21:10:14 -0500

    fixed dev error with git revert. Changed back to concat

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,31 +3,31 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  // var userInput = window.prompt("Choose password length between 8 & 128 characters");
-  // // Convert string to number
-  // var passwordLength = Number(userInput);
+  var userInput = window.prompt("Choose password length between 8 & 128 characters");
+  // Convert string to number
+  var passwordLength = Number(userInput);
 
-  // // Test if password length is a valid value
-  // if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  // Test if password length is a valid value
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
 
-  //   if (isNaN(passwordLength)) { window.alert("That is not a number!") }
-  //   else { window.alert("Length must be between 8 and 128") }
-  //   // Exit generatePassword function because invalid input
-  //   return;
-  // }
+    if (isNaN(passwordLength)) { window.alert("That is not a number!") }
+    else { window.alert("Length must be between 8 and 128") }
+    // Exit generatePassword function because invalid input
+    return;
+  }
 
-  // window.alert("Choose your password criteria ... ")
-  // // Record user input to variables as boolean values
-  // var includeLower = window.confirm("Include lowercase letters?");
-  // var includeUpper = window.confirm("Include UPPERCASE letters?");
-  // var includeNumbers = window.confirm("Include numbers?");
-  // var includeSymbols = window.confirm("Include symbols?");
+  window.alert("Choose your password criteria ... ")
+  // Record user input to variables as boolean values
+  var includeLower = window.confirm("Include lowercase letters?");
+  var includeUpper = window.confirm("Include UPPERCASE letters?");
+  var includeNumber = window.confirm("Include numbers?");
+  var includeSymbol = window.confirm("Include symbols?");
 
-  // // Verify user has selected at least one char set
-  // if (!includeLower && !includeUpper && !includeNumbers && !includeSymbols) {
-  //   window.alert("At least one character set is required!");
-  //   return;
-  // }
+  // Verify user has selected at least one char set
+  if (!includeLower && !includeUpper && !includeNumbers && !includeSymbols) {
+    window.alert("At least one character set is required!");
+    return;
+  }
 
   // Declare character set arrays
   var lowerList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -43,6 +43,9 @@ function generatePassword() {
   if (includeNumber) { selectedCriteriaList = selectedCriteriaList.concat(numberList); }
   if (includeSymbol) { selectedCriteriaList = selectedCriteriaList.concat(symbolList); }
 
+  for (var i=0; i<selectedCriteriaList.length;i++){
+    console.log(selectedCriteriaList[i]);
+  }
 
 }
 
