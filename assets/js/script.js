88776b341aed782bfce6d30fe5eb0f397ee4e183
acd74982306 <114900371+acd74982306@users.noreply.github.com>
2022-10-30 19:54:31 -0500

    removed loop in password length verification

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  var userInput = window.prompt("Choose password length: 8-128 characters");
-
-  userInput = Number(userInput);
+  var userInput = window.prompt("Choose password length between 8 & 128 characters");
+  var passwordLength = Number(userInput);
 
   // Test and reprompt user if password length is an invalid value
-  while (isNaN(userInput) || userInput < 8 || userInput > 128) {
-    userInput = window.prompt("Please enter a valid number between 8 and 128")
-    userInput = Number(userInput);
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    if (isNaN(passwordLength)) {window.alert("That is not a number!")}
+    else {window.alert("Length must be between 8 and 128")}
+    return;
   }
 
 }
